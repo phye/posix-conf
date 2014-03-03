b19985d45c7af597ee456e23a0986adb46564786
admin <admin@crdc3-lnx-1.sanity3.com>
2014-03-03 17:09:10 +0800

    Updated config.sh

--- config.sh
+++ config.sh
@@ -7,12 +7,15 @@ cp -r .vim $HOME 2>/dev/null
 echo "Appending .bashrc to include configs for phye"
 BASHRC_FILE=$HOME/.bashrc
 touch $BASHRC_FILE
-grep "bashrc_phye" $BASHRC_FILE
+grep "bashrc_phye" $BASHRC_FILE >> /dev/null
 if [ $? != 0 ]; then
     echo 'if [ -f $HOME/.bashrc_phye ]; then' >> $BASHRC_FILE 
     echo '    source $HOME/.bashrc_phye' >> $BASHRC_FILE
     echo 'fi' >> $BASHRC_FILE
+else
+    echo "Phye's config is already in .bashrc, no need to append"
 fi
 
 echo "Sourcing modified .bashrc_phye"
+echo ""
 source $BASHRC_FILE
