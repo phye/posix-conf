6fe0567347de64d996c8434a6a9a63692d3516bd
phye <semimiracle@gmail.com>
2014-03-04 10:22:22 +0800

    Updated sanity.sh with telnet support

--- phye/sanity.sh
+++ phye/sanity.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 PREFIX=''
+CMD='s'
 if [ "$1" == "2" ]; then
     PREFIX=$1
 fi
 
+if [ "$2" == "telnet" ]; then
+    CMD='t'
+fi
+
+echo $CMD
 tmux selectp -t 0
 tmux splitw -h -p 50
 
@@ -14,13 +20,13 @@ tmux splitw -v -p 50
 
 tmux selectp -t 0 
 tmux send-keys "sats2" C-m
-tmux send-keys "s$PREFIX""cdsm" C-m
+tmux send-keys "$CMD$PREFIX""cdsm" C-m
 tmux selectp -t 1 
 tmux send-keys "sats2" C-m
-tmux send-keys "s$PREFIX""sr" C-m
+tmux send-keys "$CMD$PREFIX""sr" C-m
 tmux selectp -t 2 
 tmux send-keys "sats2" C-m
-tmux send-keys "s$PREFIX""se1" C-m
+tmux send-keys "$CMD$PREFIX""se1" C-m
 tmux selectp -t 3 
 tmux send-keys "sats2" C-m
-tmux send-keys "s$PREFIX""se2" C-m
+tmux send-keys "$CMD$PREFIX""se2" C-m
