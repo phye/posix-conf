46f2388f645dbe812cfb2c262a60b82c4de41eee
phye <phye@cisco.com>
2014-03-04 10:52:02 +0800

    Update .bashrc_phye for git aliases

--- config.sh
+++ config.sh
@@ -6,16 +6,15 @@ cp -r .vim $HOME 2>/dev/null
 
 echo "Appending .bashrc to include configs for phye"
 BASHRC_FILE=$HOME/.bashrc
-touch $BASHRC_FILE
+#touch $BASHRC_FILE
 grep "bashrc_phye" $BASHRC_FILE >> /dev/null
 if [ $? != 0 ]; then
-    echo 'if [ -f $HOME/.bashrc_phye ]; then' >> $BASHRC_FILE 
-    echo '    source $HOME/.bashrc_phye' >> $BASHRC_FILE
-    echo 'fi' >> $BASHRC_FILE
+    echo '[ -f ~/.bashrc_phye ] && . ~/.bashrc_phye ' >> $BASHRC_FILE 
 else
     echo "Phye's config is already in .bashrc, no need to append"
 fi
 
 echo "Sourcing modified .bashrc_phye"
 echo ""
+echo $BASHRC_FILE
 source $BASHRC_FILE
