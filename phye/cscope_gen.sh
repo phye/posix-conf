7a34272469b7c197e812c9cab327d37828e54c6b
phye <semimiracle@gmail.com>
2014-06-17 12:28:44 +0800

    [Update]
    Add script from Edit Server

---
+++ phye/cscope_gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SRC_DIR=$1
+echo "Removing old cscope files"
+for i in $SRC_DIR/cscope.*; do
+    rm -f $i
+done
+
+echo "Generating new cscope files"
+find $SRC_DIR -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.hpp" > $SRC_DIR/cscope.files
+cd $SRC_DIR && pwd 
+cscope -b -q 
+
+echo "Done"
