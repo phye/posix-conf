2e1f591d8bb9fe197067077115eb9d43828c06ef
Philip Ye <phye@cisco.com>
2016-01-19 12:31:39 +0800

    Update v2p.sh
    To use shared sco window across many v2p sessions.

--- phye/v2p.sh
+++ phye/v2p.sh
@@ -3,14 +3,22 @@ SESSION=$1
 V2PC_ROOT=/ws/v2p-master
 SCO_ROOT=~/ws/scos
 
-cd $SCO_ROOT
-tmux -2 new-session -d -s $SESSION
-tmux -2 rename-window -t $SESSION:0 'sco' 
+tmux has-session -t shared 2>/dev/null
+if [[ $? != 0 ]]; then
+    tmux -2 new-session -d -s 'shared' -n 'sco' -c $SCO_ROOT 
+    tmux -2 detach-client -s 'shared'
+else
+    echo "shared session already exist"
+fi
 
-tmux -2 new-window -t $SESSION:1 -n 'v2p' -c $V2PC_ROOT
-tmux -2 new-window -t $SESSION:2 -n 'v2p' -c $V2PC_ROOT
-tmux -2 new-window -t $SESSION:3 -n 'v2p' -c $V2PC_ROOT
-tmux -2 new-window -t $SESSION:4 -n 'v2p' -c $V2PC_ROOT
+cd $V2PC_ROOT
+tmux -2 new-session -d -s $SESSION -n 'v2p' -c $V2PC_ROOT
+tmux -2 new-window -t $SESSION:1 -n 'v2p' 
+tmux -2 new-window -t $SESSION:2 -n 'v2p'
+tmux -2 new-window -t $SESSION:3 -n 'v2p' 
+tmux -2 new-window -t $SESSION:4 -n 'v2p'
+tmux -2 kill-window -t $SESSION:0
+tmux -2 link-window -d -s shared:sco -t 0 
 
-tmux -2 select-window -t $SESSION:0 
-tmux -2 attach-session -t $SESSION
+tmux -2 select-window -t $SESSION:0
+tmux -2 attach-session -d -t $SESSION
