1ddfa32e4f0c7c3640a6ec644b8985ecf65a58b7
Philip Ye <phye@cisco.com>
2016-08-05 21:46:14 +0800

    Update edit.sh to not name window manually

--- phye/edit.sh
+++ phye/edit.sh
@@ -2,11 +2,11 @@
 SESSION=$1
 tmux -2 new-session -d -s $SESSION
 
-tmux -2 new-window -t $SESSION:1 -n 'src' 
-tmux -2 new-window -t $SESSION:2 -n 'aux' 
-tmux -2 new-window -t $SESSION:3 -n 'checkin'
-tmux -2 new-window -t $SESSION:4 -n 'aux2'
-tmux -2 new-window -t $SESSION:5 -n 'ATS' 
+tmux -2 new-window -t $SESSION:1 
+tmux -2 new-window -t $SESSION:2 
+tmux -2 new-window -t $SESSION:3 
+tmux -2 new-window -t $SESSION:4 
+tmux -2 new-window -t $SESSION:5 
 
 tmux -2 select-window -t $SESSION:^
 tmux -2 attach-session -t $SESSION
