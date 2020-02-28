4866d63c509cfa08e8810f261dc39ed633e4bad3
Philip Ye <phye8080@gmail.com>
2020-02-28 09:34:37 +0800

    dev: update my config.sh script

--- tools/config.sh
+++ tools/config.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
-mkdir -p $HOME
-echo "Copying all config files to $HOME"
-/bin/cp -f .[a-z]* $HOME 2>/dev/null
-/bin/cp -r .vim $HOME 2>/dev/null
+# TODO(phye): replace ROOT_DIR dynamically
+GITDIR=$(pwd)
+mkdir -p ${HOME}
+mkdir -p ${HOME}/bin
 
-mkdir -p $HOME/bin/
-/bin/cp -r phye $HOME/bin/ 2>/dev/null
-/bin/cp -r sanity $HOME/bin/ 2>/dev/null
+echo "1. Configure all submodules"
+# vim
+ln -s ${GITDIR}/modules/vim.d/vim ~/.vim
+ln -s ${GITDIR}/modules/vim.d/vimrc ~/.vimrc
 
-cat ssh_config >> ~/.ssh/config
+# emacs
+ln -s ${GITDIR}/modules/emacs.d ~/.emacs.d
 
-echo "Appending .bashrc to include configs for phye"
-BASHRC_FILE=$HOME/.bashrc
-#touch $BASHRC_FILE
-grep "bashrc.phye" $BASHRC_FILE >> /dev/null
+# oh-my-zsh
+ln -s ${GITDIR}/modules/oh-my-zsh ~/.oh-my-zsh
+
+# tpm
+ln -s ${GITDIR}/modules/tpm ~/.tpm
+
+echo "2. Link all common configs"
+# git
+ln -s ${GITDIR}/configs/git/gitconfig ~/.gitconfig
+
+# shell
+ln -s ${GITDIR}/configs/shell/shrc.phye.sh ~/.shrc.phye.sh
+ln -s ${GITDIR}/configs/shell/git-completion.bash /.git-completion.bash
+ln -s ${GITDIR}/configs/shell/bashrc.phye.sh ~/.bashrc.phye.sh
+ln -s ${GITDIR}/configs/shell/zshrc.phye.sh ~/.zshrc.phye.sh
+ln -sf ${GITDIR}/configs/shell/zshrc.sh ~/.zshrc.sh
+
+# tmux
+ln -s ${GITDIR}/configs/tmux/tmux.conf ~/.tmux.conf
+
+echo "3. Patching existing configs"
+# bash
+grep "bashrc.phye.sh" ${HOME}/.bashrc >> /dev/null
 if [ $? != 0 ]; then
-    echo '[ -f ~/.bashrc.phye ] && . ~/.bashrc.phye ' >> $BASHRC_FILE 
-else
-    echo "Phye's config is already in .bashrc, no need to append"
+    echo '[ -f ~/.bashrc.phye ] && . ~/.bashrc.phye ' >> ${HOME}/.bashrc
 fi
+
+echo "4. Linking all scripts"
+ln -s ${GITDIR}/scripts ${HOME}/bin/scripts
+
+echo "5. Config all sytles"
+
+echo "!!! Enjoy !!!"
