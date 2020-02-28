5f6367b14babb94475341732f4f330c26aed19ab
Philip Ye <phye@cisco.com>
2020-02-28 21:53:27 +0800

    bugfix: working config.sh

--- tools/config.sh
+++ tools/config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Exit on any error!
+set -e
+
 # TODO(phye): replace ROOT_DIR dynamically
 GITDIR=$(pwd)
 mkdir -p ${HOME}
@@ -6,41 +9,46 @@ mkdir -p ${HOME}/bin
 
 echo "1. Configure all submodules"
 # vim
-ln -s ${GITDIR}/modules/vim.d/vim ~/.vim
-ln -s ${GITDIR}/modules/vim.d/vimrc ~/.vimrc
+[ -s ~/.vim ] && rm ~/.vim
+ln -sf ${GITDIR}/modules/vim.d/vim/ ~/.vim
+ln -sf ${GITDIR}/modules/vim.d/vimrc ~/.vimrc
 
 # emacs
-ln -s ${GITDIR}/modules/emacs.d ~/.emacs.d
+[ -d ~/.emacs.d ] && mv ~/.emacs.d ~/.emacs.d.old
+[ -s ~/.emacs.d ] && rm ~/.emacs.d
+ln -s ${GITDIR}/modules/emacs.d/ ~/.emacs.d
 
 # oh-my-zsh
-ln -s ${GITDIR}/modules/oh-my-zsh ~/.oh-my-zsh
+[ -d ~/.oh-my-zsh ] && mv ~/.oh-my-zsh ~/.on-my-zsh.old
+[ -s ~/.oh-my-zsh ] && rm ~/.oh-my-zsh
+ln -s ${GITDIR}/modules/oh-my-zsh/ ~/.oh-my-zsh
 
 # tpm
-ln -s ${GITDIR}/modules/tpm ~/.tpm
+mkdir -p ${HOME}/.tmux
+ln -sf ${GITDIR}/modules/tpm/ ~/.tmux/plugins/tpm
 
 echo "2. Link all common configs"
 # git
+[ -f ~/.gitconfig ] && mv ~/.gitconfig ~/.gitconfig.old
 ln -s ${GITDIR}/configs/git/gitconfig ~/.gitconfig
 
 # shell
-ln -s ${GITDIR}/configs/shell/shrc.phye.sh ~/.shrc.phye.sh
-ln -s ${GITDIR}/configs/shell/git-completion.bash /.git-completion.bash
-ln -s ${GITDIR}/configs/shell/bashrc.phye.sh ~/.bashrc.phye.sh
-ln -s ${GITDIR}/configs/shell/zshrc.phye.sh ~/.zshrc.phye.sh
-ln -sf ${GITDIR}/configs/shell/zshrc.sh ~/.zshrc.sh
+ln -sf ${GITDIR}/configs/shell/shrc.phye.sh ~/.shrc.phye.sh
+ln -sf ${GITDIR}/configs/shell/git-completion.bash ~/.git-completion.bash
+ln -sf ${GITDIR}/configs/shell/bashrc.phye.sh ~/.bashrc.phye.sh
+ln -sf ${GITDIR}/configs/shell/zshrc.phye.sh ~/.zshrc.phye.sh
+ln -sf ${GITDIR}/configs/shell/zshrc.sh ~/.zshrc
 
 # tmux
+[ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.old
 ln -s ${GITDIR}/configs/tmux/tmux.conf ~/.tmux.conf
 
 echo "3. Patching existing configs"
 # bash
-grep "bashrc.phye.sh" ${HOME}/.bashrc >> /dev/null
-if [ $? != 0 ]; then
-    echo '[ -f ~/.bashrc.phye ] && . ~/.bashrc.phye ' >> ${HOME}/.bashrc
-fi
+grep "bashrc.phye.sh" ${HOME}/.bashrc || echo '[ -f ~/.bashrc.phye ] && . ~/.bashrc.phye ' >> ${HOME}/.bashrc
 
 echo "4. Linking all scripts"
-ln -s ${GITDIR}/scripts ${HOME}/bin/scripts
+ln -s ${GITDIR}/scripts/ ${HOME}/bin/scripts
 
 echo "5. Config all sytles"
 
