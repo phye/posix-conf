94b988081d2a9ce4c66d77369ba7bf45bf4ad161
Philip Ye <phye@cisco.com>
2020-02-29 10:39:19 +0800

    bugfix: better apply.sh

--- tools/apply.sh
+++ tools/apply.sh
@@ -1,54 +1,55 @@
 #!/bin/bash
 # Exit on any error!
 set -e
+alias lnd='ln -sT -b'
 
 # TODO(phye): replace ROOT_DIR dynamically
 GITDIR=$(pwd)
 mkdir -p ${HOME}
 mkdir -p ${HOME}/bin
+mkdir -p ${HOME}/.tmux/plugins/
 
 echo "1. Configure all submodules"
 # vim
-[ -s ~/.vim ] && rm ~/.vim
-ln -sf ${GITDIR}/modules/vim.d/vim/ ~/.vim
-ln -sf ${GITDIR}/modules/vim.d/vimrc ~/.vimrc
+lnd -f ${GITDIR}/modules/vim.d/vim/ ~/.vim
+ln -sfb ${GITDIR}/modules/vim.d/vimrc ~/.vimrc
 
 # emacs
-[ -d ~/.emacs.d ] && mv ~/.emacs.d ~/.emacs.d.old
-[ -s ~/.emacs.d ] && rm ~/.emacs.d
-ln -s ${GITDIR}/modules/emacs.d/ ~/.emacs.d
+lnd -f ${GITDIR}/modules/emacs.d/ ~/.emacs.d
 
 # oh-my-zsh
-[ -d ~/.oh-my-zsh ] && mv ~/.oh-my-zsh ~/.on-my-zsh.old
-[ -s ~/.oh-my-zsh ] && rm ~/.oh-my-zsh
-ln -s ${GITDIR}/modules/oh-my-zsh/ ~/.oh-my-zsh
+lnd -f ${GITDIR}/modules/oh-my-zsh/ ~/.oh-my-zsh
 
 # tpm
-mkdir -p ${HOME}/.tmux
-ln -sf ${GITDIR}/modules/tpm/ ~/.tmux/plugins/tpm
+lnd -f ${GITDIR}/modules/tpm/ ~/.tmux/plugins/tpm
 
 echo "2. Link all common configs"
 # git
-[ -f ~/.gitconfig ] && mv ~/.gitconfig ~/.gitconfig.old
-ln -s ${GITDIR}/configs/git/gitconfig ~/.gitconfig
+ln -sfb ${GITDIR}/configs/git/gitconfig ~/.gitconfig
 
 # shell
-ln -sf ${GITDIR}/configs/shell/shrc.phye.sh ~/.shrc.phye.sh
-ln -sf ${GITDIR}/configs/shell/git-completion.bash ~/.git-completion.bash
-ln -sf ${GITDIR}/configs/shell/bashrc.phye.sh ~/.bashrc.phye.sh
-ln -sf ${GITDIR}/configs/shell/zshrc.phye.sh ~/.zshrc.phye.sh
-ln -sf ${GITDIR}/configs/shell/zshrc.sh ~/.zshrc
+ln -sfb ${GITDIR}/configs/shell/shrc.phye.sh ~/.shrc.phye.sh
+ln -sfb ${GITDIR}/configs/shell/git-completion.bash ~/.git-completion.bash
+ln -sfb ${GITDIR}/configs/shell/bashrc.phye.sh ~/.bashrc.phye.sh
+ln -sfb ${GITDIR}/configs/shell/zshrc.phye.sh ~/.zshrc.phye.sh
+ln -sfb ${GITDIR}/configs/shell/zshrc.sh ~/.zshrc
 
 # tmux
-[ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.old
-ln -s ${GITDIR}/configs/tmux/tmux.conf ~/.tmux.conf
+ln -sfb ${GITDIR}/configs/tmux/tmux.conf ~/.tmux.conf
 
 echo "3. Patching existing configs"
 # bash
-grep "bashrc.phye.sh" ${HOME}/.bashrc || echo '[ -f ~/.bashrc.phye ] && . ~/.bashrc.phye ' >> ${HOME}/.bashrc
+if grep -q "bashrc.phye.sh" ${HOME}/.bashrc; then
+    echo "Already patched bashrc, continue"
+else
+    echo -e "\n[ -f .bashrc.phye.sh ] && source .bashrc.phye.sh" >> ${HOME}/.bashrc
+fi
+#if [[ $(ls -al) -ne 0 ]]; then
+#    echo "hi"
+#fi
 
 echo "4. Linking all scripts"
-ln -s ${GITDIR}/scripts/ ${HOME}/bin/scripts
+lnd -f ${GITDIR}/scripts/ ${HOME}/bin/scripts
 
 echo "5. Config all sytles"
 
