f380a3daa777f5da54fcc7ed3255eb4a2fb5aabc
Philip Ye <phye@cisco.com>
2020-03-02 01:00:33 +0800

    dev: add fzf util

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -42,6 +42,7 @@ function set_emacs_alias() {
     alias emacs="LC_CTYPE=zh_CN.UTF-8 emacs"
     alias ed='emacs --daemon=$(tmux_get_session_name)'
     alias ec='emacsclient -nw --socket=$(tmux_get_session_name)'
+    alias ecf='ec $(fzf)'
     # }}}
 }
 
