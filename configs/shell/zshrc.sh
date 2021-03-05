e4af9309b3ae8b60ebefbfde801e948e781daf76
Philip Ye <phye@tencent.com>
2021-03-05 17:11:28 +0800

    dev: update configs

--- configs/shell/zshrc.sh
+++ configs/shell/zshrc.sh
@@ -49,7 +49,7 @@ ZSH_THEME="phye"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git git-flow tmux virtualenv emacs golang man)
+plugins=(git git-flow gitfast tmux virtualenv emacs golang man)
 
 # User configuration
 
