ddfd3eda7152de531fddb82b2f73f2e65301acb1
Philip Ye <phye@tencent.com>
2021-04-18 12:09:49 +0800

    dev: add hidpi support

--- configs/shell/env.phye.sh
+++ configs/shell/env.phye.sh
@@ -9,6 +9,13 @@ export PATH=$PATH:$GOPATH/bin
 export PIPDIR=$(pip3 show powerline-status | perl -nwl -e 'if (m/Location: (.*)/) {print $1}')
 export LD_LIBRARY_PATH="/usr/local/lib:"
 export MANPATH=":/usr/local/share/man"
+
+# hidpi support
+# qt 5
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+# gtk 3
+export GDK_SCALE=2
+export GDK_DPI_SCALE=0.5
 # }}}
 
 PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
