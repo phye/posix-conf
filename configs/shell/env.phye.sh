91e30f01dcc1f320f891c7970e3237c232170598
Philip Ye <phye@tencent.com>
2021-05-23 16:53:10 +0800

    bugfix: cleanup env

--- configs/shell/env.phye.sh
+++ configs/shell/env.phye.sh
@@ -1,14 +1,17 @@
 # ENVs {{{
 export SCRIPTS=${HOME}/bin/scripts
-export PATH="$HOME/bin/scripts:$HOME/bin:/snap/bin:$PATH"
 export EDITOR='vim'
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export GOPATH=~/ws/go
-export PATH=$PATH:$GOPATH/bin
-export PIPDIR="$(pip3 show powerline-status | perl -nwl -e 'if (m/Location: (.*)/) {print $1}')"
+export PATH="$HOME/bin/scripts:$HOME/bin:$GOPATH/bin:/snap/bin:/usr/local/bin:/usr/local/opt/fzf/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin:/opt/X11/bin:/Library/Apple/usr/bin"
 export LD_LIBRARY_PATH="/usr/local/lib:"
 export MANPATH=":/usr/local/share/man"
+export PIPDIR="$(pip3 show powerline-status | perl -nwl -e 'if (m/Location: (.*)/) {print $1}')"
+export GUILE_LOAD_PATH="/usr/local/share/guile/site/3.0"
+export GUILE_LOAD_COMPILED_PATH="/usr/local/lib/guile/3.0/site-ccache"
+export GUILE_SYSTEM_EXTENSIONS_PATH="/usr/local/lib/guile/3.0/extensions"
+
 
 # hidpi support
 # qt 5
@@ -18,4 +21,5 @@ export GDK_SCALE=2
 export GDK_DPI_SCALE=0.5
 # }}}
 
-PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
+# disable path unify
+# PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
