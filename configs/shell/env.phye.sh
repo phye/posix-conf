4d65343c6b8b40b15546b455e882690913ee22b1
Philip Ye <phye@tencent.com>
2022-06-10 22:50:57 +0800

    update configsg

--- configs/shell/env.phye.sh
+++ configs/shell/env.phye.sh
@@ -18,8 +18,8 @@ export PIPDIR=`pip3 show powerline-status | perl -nwl -e 'if (m/Location: (.*)/)
 # qt 5
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 # gtk 3
-export GDK_SCALE=2
-export GDK_DPI_SCALE=0.5
+# export GDK_SCALE=2
+# export GDK_DPI_SCALE=0.5
 # }}}
 
 # disable path unify
