51e07c3692be5300b04a00605f48a107da2c9e0a
Philip Ye <phye.pro@gmail.com>
2024-04-15 21:26:28 +0800

    daily bug fixes

--- configs/shell/env.phye.sh
+++ configs/shell/env.phye.sh
@@ -1,6 +1,6 @@
 # ENVs {{{
 export SCRIPTS=${HOME}/bin/scripts
-export EDITOR='emacsclient -nw -a'
+export EDITOR="emacsclient -nw -a ''"
 export LC_ALL=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 export LANG=en_US.UTF-8
