f4f134edd6c0b4bab6072e7cfd5bf48cbf53c24c
Philip Ye <phye@tencent.com>
2024-04-25 07:29:26 +0800

    more logic in var.sh

--- configs/shell/var.sh
+++ configs/shell/var.sh
@@ -1,13 +1,20 @@
 # This file stores machine dependent configs
 
-#export http_proxy=http://127.0.0.1:8123
-#export https_proxy=$http_proxy
-#export no_proxy=127.0.0.1,mirrors.tencentyun.com,mirrors.tencent.com,mirrors.163.com,mirrors.tuna.tsinghua.edu.cn
-
 # locations (for location specific configs in zsh and emacs)
-export LOCATION=office
+export LOCATION="home"
+
+# proxy
+if [ "$LOCATION" = "home" ]; then
+    export http_proxy=http://127.0.0.1:8123
+    export https_proxy=$http_proxy
+    export no_proxy=127.0.0.1,mirrors.tencentyun.com,mirrors.tencent.com,mirrors.163.com,mirrors.tuna.tsinghua.edu.cn
+fi
 
 # utils
-export default_bin_path=/opt/homebrew/bin
+if [ `uname -s` = "Darwin" ]; then
+    export default_bin_path=/opt/homebrew/bin
+else
+    export default_bin_path=/usr/local/bin
+fi
 export ZSH_PATH=$default_bin_path/zsh
 export FZF_PATH=$default_bin_path/fzf
