94dd715f3e7d0e51d83f1a5de64b4b17ec3974fa
Philip Ye <phye@tencent.com>
2024-04-25 09:59:46 +0800

    remove proxy.sh

--- configs/shell/proxy.sh
+++
@@ -1,3 +0,0 @@
-export http_proxy=http://127.0.0.1:8123
-export https_proxy=$http_proxy
-export no_proxy=127.0.0.1,mirrors.tencentyun.com,mirrors.tencent.com,mirrors.163.com,mirrors.tuna.tsinghua.edu.cn
