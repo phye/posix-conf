cbfb6d1c91052674d783ff03ef3b42c5d974dd10
Philip Ye <phye.pro@gmail.com>
2024-05-09 10:24:10 +0800

    only trigger system level dark theme for mac

--- scripts/toggle_dark_theme.sh
+++ scripts/toggle_dark_theme.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-if [ $LOCATION = "office" ]; then
-   osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to not dark mode'
+if [[ "$LOCATION" == "office" ]]; then
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        /usr/bin/osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to not dark mode'
+    fi
 fi
