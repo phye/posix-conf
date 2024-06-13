fcfd7e36e7be4d3dbe5d39ff1ec66e4943758f9b
Philip Ye <phye@tencent.com>
2024-06-13 16:49:15 +0800

    use space for alias expand

--- configs/shell/zshrc.phye.sh
+++ configs/shell/zshrc.phye.sh
@@ -15,8 +15,11 @@ globalias() {
    zle self-insert
 }
 zle -N globalias
-bindkey -M emacs "\t" globalias
-bindkey -M viins "\t" globalias
+
+bindkey -M emacs " " globalias
+bindkey -M viins " " globalias
+bindkey -M emacs "^ " magic-space
+bindkey -M viins "^ " magic-space
 
 hash -d conf=~/ws/posix-conf
 hash -d gtd=~/ws/gtd
