06d09873b0b3f020965a94b4d8664b067fcc877e
Philip Ye <phye@tencent.com>
2025-09-26 16:05:49 +0800

    use ec.sh for restore terminal settings on emacsclient exit with error

---
+++ scripts/ec.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Wrapper for emacsclient to reset terminal settings with stty
+
+# Run emacsclient with provided arguments
+emacsclient -nw "$@"
+exit_code=$?
+
+# Check if emacsclient exited with non-zero status
+if [ $exit_code -ne 0 ]; then
+    # Reset terminal settings
+    stty sane
+fi
+
+# Return the exit code of emacsclient
+exit $exit_code
