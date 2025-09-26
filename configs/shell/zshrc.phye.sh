0686daf7057159dd1be25d594f61dea9b72170eb
Philip Ye <phye@tencent.com>
2025-09-26 15:38:27 +0800

    update settings

--- configs/shell/zshrc.phye.sh
+++ configs/shell/zshrc.phye.sh
@@ -1,25 +1,20 @@
 # zsh dir alias
 hash -d conf=~/ws/posix-conf
 hash -d gtd=~/ws/gtd
-hash -d gows=~/ws/go/src/github.com/phye
 hash -d ebooks=~/Documents/OneDrive/Ebooks
-hash -d media=~/ws/yt-media-ai-videounderstanding
-hash -d public=~/ws/yt-public-cloud
-hash -d biz=~/ws/yt-industry-ai
-hash -d proto=~/ws/yt-industry-ai/proto
-hash -d common=~/ws/yt-industry-ai/yt-server-common
-hash -d gocommon=~/ws/yt-industry-ai/yt-go-common
 hash -d gitwoa=~/ws/git.code
 hash -d github=~/ws/github
 hash -d emacs=~/.emacs.d
 hash -d emacsdata=~/.emacs.data.d
 hash -d ssd=/Volumes/phye-ssd
 hash -d onedrive=~/Documents/OneDrive
-hash -d legacy=~/ws/legacy
-hash -d trpc=~/ws/git.code/trpc
+#hash -d legacy=~/ws/legacy
+hash -d trpc=~/ws/trpc
 hash -d ti=~/ws/ti-cloud
 hash -d infra=~/ws/ti-cloud/infra
 hash -d k8stotal=~/ws/github/k8s.total
+hash -d adp=~/ws/adp
+hash -d legacy=~/ws/adp/legacy
 
 function load_if_exist () {
     [ -f $1 ] && source $1
