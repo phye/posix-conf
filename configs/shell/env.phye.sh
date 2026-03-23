bebf5f260ec7a6710e764ce8859c1ce0b1a921e8
Philip Ye <phye.pro@gmail.com>
2026-03-23 14:20:55 +0800

    source cred in env

--- configs/shell/env.phye.sh
+++ configs/shell/env.phye.sh
@@ -1,6 +1,7 @@
 # ENVs {{{
 [ -f "$HOME/.cargo/env" ] && source "$HOME/.cargo/env"
 [ -f "$HOME/.var.sh" ] && source "$HOME/.var.sh"
+[ -f "$HOME/.cred.sh" ] && source "$HOME/.cred.sh"
 
 export LC_ALL=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
